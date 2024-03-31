2b33e7baf5aaa541a4b71f08b89f99efd0e1281b
junseoplee <junseoplee@soongsil.ac.kr>
2024-03-31 15:22:50 +0900

    test: position 이 올바르게 설정되는지 확인하는 테스트 구현

---
+++ src/test/java/chess/model/position/PositionTest.java
@@ -0,0 +1,25 @@
+package chess.model.position;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PositionTest {
+
+  @Test
+  @DisplayName("포지션_객체_생성_시_파일과_랭크가_올바르게_설정되어야_한다")
+  void 포지션_객체_생성_시_파일과_랭크가_올바르게_설정되어야_한다() {
+    //given
+    char fileChar = 'c';
+    int rankNumber = 4;
+
+    //when
+    Position position = new Position(fileChar, rankNumber);
+
+    //then
+    assertThat(position.getFile()).isEqualTo(File.FILE_C);
+    assertThat(position.getRank()).isEqualTo(Rank.RANK_4);
+  }
+}
