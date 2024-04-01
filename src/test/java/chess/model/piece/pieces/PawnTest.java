f1a9a47047e5fdc38ec636985576dcb286823a71
junseoplee <junseoplee@soongsil.ac.kr>
2024-04-01 23:49:19 +0900

    test: 폰 초기화 테스트 구현

---
+++ src/test/java/chess/model/piece/pieces/PawnTest.java
@@ -0,0 +1,25 @@
+package chess.model.piece.pieces;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import chess.model.position.Color;
+import chess.model.position.Position;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PawnTest {
+  @Test
+  @DisplayName("폰_초기화_테스트")
+  void 폰_초기화_테스트() {
+    //given
+    Color color = Color.WHITE;
+    Position position = new Position('a', 2);
+
+    //when
+    Pawn pawn = new Pawn(color, position);
+
+    //then
+    assertThat(pawn.getColor()).isEqualTo(color);
+    assertThat(pawn.getPosition()).isEqualTo(position);
+  }
+}
