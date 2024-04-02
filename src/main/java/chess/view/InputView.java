009e04941fdd78118554601d8d7a49fe7a93d7cc
junseoplee <junseoplee@soongsil.ac.kr>
2024-04-03 02:40:44 +0900

    feat: 체스판과 피스의 심볼을 지정하고 출력하는 메서드 구현

--- src/main/java/chess/view/InputView.java
+++ src/main/java/chess/view/InputView.java
@@ -7,10 +7,7 @@ public class InputView {
 
   private static final Scanner scanner = new Scanner(System.in);
 
-  public Command receiveStartCommand() {
-    System.out.println("체스 게임을 시작합니다.");
-    System.out.println("게임 시작은 start, 종료는 end 명령을 입력하세요.");
-
+  public Command receiveCommand() {
     String inputCommand = scanner.nextLine();
     return new Command(inputCommand);
   }
