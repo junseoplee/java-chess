516aed089cb559055e22c83a5e3343e3c4428362
junseoplee <junseoplee@soongsil.ac.kr>
2024-04-03 02:42:28 +0900

    feat: 체스 애플리케이션 구현

--- src/main/java/chess/ChessApplication.java
+++ src/main/java/chess/ChessApplication.java
@@ -1,8 +1,19 @@
 package chess;
 
+import chess.controller.ChessController;
+import chess.model.board.InitialBoard;
+import chess.view.InputView;
+import chess.view.OutputView;
+
 public class ChessApplication {
 
   public static void main(String[] args) {
 
+    InputView inputView = new InputView();
+    OutputView outputView = new OutputView();
+    InitialBoard initialBoard = new InitialBoard();
+
+    ChessController chessController = new ChessController(inputView, outputView, initialBoard);
+    chessController.runChess();
   }
 }
