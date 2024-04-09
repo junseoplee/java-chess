4f5aa6cfc95659edf1c073809376b45aafbeead7
junseoplee <junseoplee@soongsil.ac.kr>
2024-04-09 20:03:08 +0900

    test: 클래스 목적에 맞는 테스트 코드로 수정

--- src/test/java/chess/model/board/InitialBoardTest.java
+++ src/test/java/chess/model/board/InitialBoardTest.java
@@ -1,30 +1,43 @@
 package chess.model.board;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import chess.model.piece.Piece;
-import chess.model.piece.pieces.King;
+import chess.model.piece.pieces.*;
 import chess.model.position.Color;
-import chess.model.position.InitialPosition;
+import chess.model.position.Position;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class InitialBoardTest {
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Map;
+
+public class InitialBoardTest {
 
   @Test
-  @DisplayName("블랙_킹이_올바른_위치에_있는지_확인")
-  void 블랙_킹이_올바른_위치에_있는지_확인() {
-    //given
+  @DisplayName("초기 보드에는 정확히 32개의 말이 있어야 한다")
+  public void 초기_보드에는_정확히_32개의_말이_있어야_한다() {
     InitialBoard initialBoard = new InitialBoard();
-
-    //when
     Board board = initialBoard.createInitialBoard();
-    Piece pieceAtBlackKingPosition = board.getMap().get(InitialPosition.BLACK_KING.getPosition());
+    Map<Position, Piece> boardMap = board.getMap();
 
-    //then
-    assertThat(pieceAtBlackKingPosition)
-        .isInstanceOf(King.class)
-        .extracting("color")
-        .isEqualTo(Color.BLACK);
+    assertEquals(32, boardMap.size());
+  }
+
+  @Test
+  @DisplayName("초기 보드에는 각 말의 정확한 수가 있어야 한다")
+  public void 초기_보드에는_각_말의_정확한_수가_있어야_한다() {
+    InitialBoard initialBoard = new InitialBoard();
+    Board board = initialBoard.createInitialBoard();
+    Map<Position, Piece> boardMap = board.getMap();
+
+    long whitePawns = boardMap.values().stream().filter(piece -> piece instanceof Pawn && piece.getColor() == Color.WHITE).count();
+    long blackPawns = boardMap.values().stream().filter(piece -> piece instanceof Pawn && piece.getColor() == Color.BLACK).count();
+    assertEquals(8, whitePawns);
+    assertEquals(8, blackPawns);
+
+    long whiteKnights = boardMap.values().stream().filter(piece -> piece instanceof Knight && piece.getColor() == Color.WHITE).count();
+    long blackKnights = boardMap.values().stream().filter(piece -> piece instanceof Knight && piece.getColor() == Color.BLACK).count();
+    assertEquals(2, whiteKnights);
+    assertEquals(2, blackKnights);
   }
 }
