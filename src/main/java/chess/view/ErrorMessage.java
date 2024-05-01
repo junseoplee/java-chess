f879c9d85dfd2ea56775cfd5a8a9248d1d525dad
junseoplee <junseoplee@soongsil.ac.kr>
2024-05-01 11:20:47 +0900

    feat: 경로에 다른 체스 말이 있다면 예외 처리하는 기능 구현
    (cherry picked from commit 6507583ba5bcdc07e0aac32b7ac078d59d73bca7)

--- src/main/java/chess/view/ErrorMessage.java
+++ src/main/java/chess/view/ErrorMessage.java
@@ -4,7 +4,9 @@ public enum ErrorMessage {
   MISMATCH_COMMAND("[ERROR] 명령은 start, end 중 하나여야 합니다."),
   MISMATCH_FILE_ARGUMENT("[ERROR] 잘못된 파일 규격입니다."),
   MISMATCH_RANK_ARGUMENT("[ERROR] 잘못된 랭크 규격입니다."),
-  MISMATCH_SYMBOL("[ERROR] 잘못된 피스 타입입니다.");
+  MISMATCH_SYMBOL("[ERROR] 잘못된 피스 타입입니다."),
+  HAS_OBSTACLE("[ERROR] 경로에 다른 체스 말이 존재합니다."),
+  MISMATCH_MOVEMENT("[ERROR] 찾을 수 없는 움직임입니다.");
 
   private final String message;
 
