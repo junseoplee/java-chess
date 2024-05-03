5214ac32ffab23500a46373eeff54c546258bda6
junseoplee <junseoplee@soongsil.ac.kr>
2024-05-03 12:42:49 +0900

    feat: 출발지와 목적지의 차이를 계산하여 이동 방향을 도출하는 기능 구현

--- src/test/java/chess/model/position/PositionTest.java
+++ src/test/java/chess/model/position/PositionTest.java
@@ -1,8 +1,9 @@
 package chess.model.position;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
+import chess.model.movement.Movement;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -21,5 +22,65 @@ class PositionTest {
     //then
     assertThat(position.getFile()).isEqualTo(File.C);
     assertThat(position.getRank()).isEqualTo(Rank.RANK_4);
+  }
+
+  @Test
+  @DisplayName("동일한_위치로_이동한다면_예외_처리한다")
+  void 동일한_위치로_이동한다면_예외_처리한다() {
+    // given
+    Position from = new Position(3, 4);
+    Position to = new Position(3, 4);
+
+    // when & then
+    assertThatThrownBy(() -> to.convertMovement(from))
+        .isInstanceOf(IllegalArgumentException.class);
+  }
+
+  @Test
+  @DisplayName("유효하지_않은_방향으로_이동한다면_예외_처리한다")
+  void 유효하지_않은_방향으로_이동한다면_예외_처리한다() {
+    // given
+    Position from = new Position(3, 4);
+    Position to = new Position(8, 7);
+
+    // when & then
+    assertThatThrownBy(() -> to.convertMovement(from))
+        .isInstanceOf(IllegalArgumentException.class);
+  }
+
+  @Test
+  @DisplayName("올바르게_이동_방향을_계산하고_찾는지_테스트")
+  void 올바르게_이동_방향을_계산하고_찾는지_테스트() {
+    //given
+    Position from = new Position(1, 1);
+    Position to = new Position(2, 3);
+    //when
+    Movement movement = to.convertMovement(from);
+    //then
+    assertThat(movement).isEqualTo(Movement.UP_UP_RIGHT);
+  }
+
+  @Test
+  @DisplayName("올바르게_이동_방향을_계산하고_찾는지_테스트2")
+  void 올바르게_이동_방향을_계산하고_찾는지_테스트2() {
+    //given
+    Position from = new Position(1, 1);
+    Position to = new Position(1, 7);
+    //when
+    Movement movement = to.convertMovement(from);
+    //then
+    assertThat(movement).isEqualTo(Movement.UP);
+  }
+
+  @Test
+  @DisplayName("올바르게_이동_방향을_계산하고_찾는지_테스트3")
+  void 올바르게_이동_방향을_계산하고_찾는지_테스트3() {
+    //given
+    Position from = new Position(1, 1);
+    Position to = new Position(7, 1);
+    //when
+    Movement movement = to.convertMovement(from);
+    //then
+    assertThat(movement).isEqualTo(Movement.RIGHT);
   }
 }
