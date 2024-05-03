0c89de76f1fadf295d8fb046d7b4ccb0c7271dab
junseoplee <junseoplee@soongsil.ac.kr>
2024-05-03 12:26:07 +0900

    feat: 열거형 상수 이름 수정 및 파일과 랭크의 차이를 입력 받아 해당 움직임을 리턴하는 기능 구현
    (cherry picked from commit 38e94926953794f8e21e3c513c2d368a2946b3b5)

--- src/main/java/chess/model/movement/Movement.java
+++ src/main/java/chess/model/movement/Movement.java
@@ -3,6 +3,8 @@ package chess.model.movement;
 import chess.model.position.File;
 import chess.model.position.Position;
 import chess.model.position.Rank;
+import chess.view.ErrorMessage;
+import java.util.Arrays;
 
 public enum Movement {
   UP(0, 1),
@@ -15,14 +17,14 @@ public enum Movement {
   DOWN_LEFT(-1, -1),
   DOWN_RIGHT(1, -1),
 
-  L_UP_LEFT(-1, 2),
-  L_UP_RIGHT(1, 2),
-  L_DOWN_LEFT(-1, -2),
-  L_DOWN_RIGHT(1, -2),
-  L_LEFT_UP(-2, 1),
-  L_LEFT_DOWN(-2, -1),
-  L_RIGHT_UP(2, 1),
-  L_RIGHT_DOWN(2, -1);
+  UP_UP_LEFT(-1, 2),
+  UP_UP_RIGHT(1, 2),
+  DOWN_DOWN_LEFT(-1, -2),
+  DOWN_DOWN_RIGHT(1, -2),
+  LEFT_LEFT_UP(-2, 1),
+  LEFT_LEFT_DOWN(-2, -1),
+  RIGHT_RIGHT_UP(2, 1),
+  RIGHT_RIGHT_DOWN(2, -1);
 
   private final int fileChange;
   private final int rankChange;
@@ -30,6 +32,13 @@ public enum Movement {
   Movement(int fileChange, int rankChange) {
     this.fileChange = fileChange;
     this.rankChange = rankChange;
+  }
+
+  public static Movement findMovement(final int file, final int rank) {
+    return Arrays.stream(Movement.values())
+                 .filter(movement -> movement.fileChange == file && movement.rankChange == rank)
+                 .findAny()
+                 .orElseThrow(() -> new IllegalArgumentException(ErrorMessage.INVALID_DIRECTION.getMessage()));
   }
 
   public Position findNextPositionFrom(File file, Rank rank) {
