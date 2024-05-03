9be94bd0af5818847d1a24728de986f2b2a5cd46
junseoplee <junseoplee@soongsil.ac.kr>
2024-05-03 12:42:49 +0900

    feat: 출발지와 목적지의 차이를 계산하여 이동 방향을 도출하는 기능 구현
    (cherry picked from commit 5214ac32ffab23500a46373eeff54c546258bda6)

--- src/main/java/chess/model/position/Position.java
+++ src/main/java/chess/model/position/Position.java
@@ -1,13 +1,48 @@
 package chess.model.position;
 
-public class Position {
+import chess.model.movement.Movement;
+import chess.view.ErrorMessage;
 
+public class Position {
   private final File file;
   private final Rank rank;
 
   public Position(final int file, final int rank) {
     this.file = File.from(file);
     this.rank = Rank.from(rank);
+  }
+
+  public Movement convertMovement(Position from) {
+    int fileDiff = this.file.getValue() - from.getFile().getValue();
+    int rankDiff = this.rank.getValue() - from.getRank().getValue();
+
+    validateSamePosition(fileDiff, rankDiff);
+
+    int gcd = getGreatestCommonDivisor(Math.abs(fileDiff), Math.abs(rankDiff));
+
+    Movement movement = Movement.findMovement(fileDiff / gcd, rankDiff / gcd);
+    if (movement != null) {
+      return movement;
+    }
+
+    throw new IllegalArgumentException(ErrorMessage.INVALID_DIRECTION.getMessage());
+  }
+
+  private int getGreatestCommonDivisor(int a, int b) {
+    if (b == 0) {
+      return a;
+    }
+    return getGreatestCommonDivisor(b, a % b);
+  }
+
+  private void validateSamePosition(int fileDiff, int rankDiff) {
+    if (fileDiff == 0 && rankDiff == 0) {
+      throw new IllegalArgumentException(ErrorMessage.SAME_POSITION.getMessage());
+    }
+  }
+
+  public Position calculateNextPosition(Movement movement) {
+    return movement.findNextPositionFrom(file, rank);
   }
 
   public File getFile() {
