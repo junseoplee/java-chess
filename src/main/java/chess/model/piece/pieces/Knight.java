ffac4b1ab562ad06ec61d6c3e4b1a786cada5a32
junseoplee <junseoplee@soongsil.ac.kr>
2024-05-04 13:06:06 +0900

    feat: 나이트의 가능한 움직임인지 유효성을 검사하고 경로를 반환하는 기능 구현
    (cherry picked from commit ab641e48f7912c4a9af70411b498775985c9bb5f)

--- src/main/java/chess/model/piece/pieces/Knight.java
+++ src/main/java/chess/model/piece/pieces/Knight.java
@@ -1,11 +1,39 @@
 package chess.model.piece.pieces;
 
+import chess.model.movement.Movement;
+import chess.model.movement.Path;
 import chess.model.piece.Piece;
 import chess.model.position.Color;
+import chess.model.position.Position;
+import chess.view.ErrorMessage;
+
+import java.util.List;
 
 public class Knight extends Piece {
 
+  private static final List<Movement> availableMovements = List.of(
+      Movement.UP_UP_LEFT, Movement.UP_UP_RIGHT,
+      Movement.DOWN_DOWN_LEFT, Movement.DOWN_DOWN_RIGHT,
+      Movement.LEFT_LEFT_UP, Movement.LEFT_LEFT_DOWN,
+      Movement.RIGHT_RIGHT_UP, Movement.RIGHT_RIGHT_DOWN);
+
   public Knight(final Color color) {
     super(color);
+  }
+
+  @Override
+  public Path findPath(Position from, Position to) {
+    Movement movement = to.convertMovement(from);
+    validateMovement(movement, availableMovements);
+
+    validateAvailableDestination(from, to, movement);
+
+    return new Path(List.of(to));
+  }
+
+  private void validateAvailableDestination(final Position from, final Position to, final Movement movement) {
+    if (!from.calculateNextPosition(movement).equals(to)) { // 뛰어 넘을 수 있어 목적지만 판단
+      throw new IllegalStateException(ErrorMessage.INVALID_DIRECTION.getMessage());
+    }
   }
 }
