412776061de05accc2b3b106d144e87aaf8da2f5
junseoplee <junseoplee@soongsil.ac.kr>
2024-05-04 23:56:45 +0900

    feat: 체스 말을 움직이는 기능 구현

--- src/main/java/chess/model/board/Board.java
+++ src/main/java/chess/model/board/Board.java
@@ -1,7 +1,10 @@
 package chess.model.board;
 
+import chess.model.movement.Path;
 import chess.model.piece.Piece;
+import chess.model.position.Color;
 import chess.model.position.Position;
+import chess.view.ErrorMessage;
 import java.util.Map;
 
 public class Board {
@@ -10,6 +13,39 @@ public class Board {
 
   public Board(Map<Position, Piece> board) {
     this.board = board;
+  }
+
+  public void move(final Position from, final Position to, final Color turn) {
+    validateMove(from, to, turn);
+    movePiece(from, to);
+  }
+
+  private void validateMove(final Position from, final Position to, final Color turn) {
+    validateIsFromEmpty(from);
+    validateIsDifferentColor(from, turn);
+
+    Piece destination = board.getOrDefault(to, Piece.EMPTY);
+    board.get(from).validateSameColor(destination);
+
+    Path path = board.get(from).findPath(from, to);
+    path.validatePath(board.keySet());
+  }
+
+  private void validateIsFromEmpty(final Position from) {
+    if (!board.containsKey(from)) {
+      throw new IllegalArgumentException(ErrorMessage.NO_PIECE_AT_SOURCE.getMessage());
+    }
+  }
+
+  private void validateIsDifferentColor(final Position from, final Color turn) {
+    if (!board.get(from).getColor().isDifferentColor(turn)) {
+      throw new IllegalArgumentException(ErrorMessage.INVALID_TURN.getMessage());
+    }
+  }
+
+  private void movePiece(final Position from, final Position to) {
+    Piece piece = board.remove(from);
+    board.put(to, piece);
   }
 
   public Map<Position, Piece> getMap() {
--- src/test/java/chess/model/board/BoardTest.java
+++ src/test/java/chess/model/board/BoardTest.java
@@ -1,33 +1,55 @@
 package chess.model.board;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import chess.model.piece.Piece;
 import chess.model.piece.pieces.Pawn;
 import chess.model.position.Color;
 import chess.model.position.InitialPosition;
 import chess.model.position.Position;
+import chess.view.ErrorMessage;
 import java.util.HashMap;
 import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class BoardTest {
 
+  private Board board;
+
+  @BeforeEach
+  void setUp() {
+    Map<Position, Piece> initialBoard = new HashMap<>();
+    board = new Board(initialBoard);
+  }
+
   @Test
   @DisplayName("보드_테스트")
   void 보드_테스트() {
-    //given
-    Map<Position, Piece> initialBoard = new HashMap<>();
+    // given
     Position position = InitialPosition.BLACK_PAWN_F.getPosition();
     Piece pawn = new Pawn(Color.BLACK);
 
-    //when
-    initialBoard.put(position, pawn);
-    Board board = new Board(initialBoard);
+    // when
+    board.getMap().put(position, pawn);
     Map<Position, Piece> retrievedMap = board.getMap();
 
-    //then
+    // then
     assertThat(retrievedMap).containsEntry(position, pawn);
+  }
+
+  @Test
+  @DisplayName("출발점에_말이_없으면_예외_발생")
+  void 출발점에_말이_없으면_예외_발생() {
+    // given
+    Position from = new Position(1, 1);
+    Position to = new Position(1, 2);
+
+    // when & then
+    assertThatThrownBy(() -> board.move(from, to, Color.WHITE))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessageContaining(ErrorMessage.NO_PIECE_AT_SOURCE.getMessage());
   }
 }
