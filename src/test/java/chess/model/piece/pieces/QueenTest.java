d450116426207daad8a4ca713742982ce0e24629
junseoplee <junseoplee@soongsil.ac.kr>
2024-05-04 12:32:22 +0900

    feat: 퀸의 가능한 움직임인지 유효성을 검사하고 경로를 반환하는 기능 구현

--- src/test/java/chess/model/piece/pieces/QueenTest.java
+++ src/test/java/chess/model/piece/pieces/QueenTest.java
@@ -1,8 +1,11 @@
 package chess.model.piece.pieces;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import chess.model.position.Color;
+import chess.model.position.Position;
+import chess.view.ErrorMessage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -19,5 +22,18 @@ class QueenTest {
 
     //then
     assertThat(queen.getColor()).isEqualTo(color);
+  }
+
+  @Test
+  @DisplayName("올바르지_않은_경로인_경우_예외_처리한다")
+  void 올바르지_않은_경로인_경우_예외_처리한다() {
+    //given
+    Queen queen = new Queen(Color.WHITE);
+    Position from = new Position(4, 4);
+    Position to = new Position(6, 5);
+    // when & then
+    assertThatThrownBy(() -> queen.findPath(from, to))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessageContaining(ErrorMessage.INVALID_DIRECTION.getMessage());
   }
 }
