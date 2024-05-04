226215efd8692dfb769f324d8aa3444d4199d47b
junseoplee <junseoplee@soongsil.ac.kr>
2024-05-04 22:16:39 +0900

    feat: 폰의 가능한 움직임인지 유효성을 검사하고 경로를 반환하는 기능 구현
    (cherry picked from commit ff6be143d2fb87af36d337cff3fb1d9679fcd78d)

--- src/test/java/chess/model/piece/pieces/PawnTest.java
+++ src/test/java/chess/model/piece/pieces/PawnTest.java
@@ -1,10 +1,17 @@
 package chess.model.piece.pieces;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import chess.model.movement.Path;
 import chess.model.position.Color;
+import chess.model.position.Position;
+import chess.view.ErrorMessage;
+import org.assertj.core.api.InstanceOfAssertFactories;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class PawnTest {
   @Test
@@ -18,5 +25,95 @@ class PawnTest {
 
     //then
     assertThat(pawn.getColor()).isEqualTo(color);
+  }
+
+  @ParameterizedTest
+  @CsvSource({
+      "2, 2, 2, 3",
+      "2, 7, 2, 6"
+  })
+  @DisplayName("폰은_앞으로_한_칸_움직일_수_있다")
+  void 폰은_앞으로_한_칸_움직일_수_있다(int fromFile, int fromRank, int toFile, int toRank) {
+    // given
+    Pawn pawn = new Pawn(fromRank == 2 ? Color.WHITE : Color.BLACK);
+    Position from = new Position(fromFile, fromRank);
+    Position to = new Position(toFile, toRank);
+
+    // when
+    Path path = pawn.findPath(from, to);
+
+    // then
+    assertThat(path)
+        .extracting("positions", InstanceOfAssertFactories.list(Position.class))
+        .containsExactly(to);
+  }
+
+  @DisplayName("폰은_초기_위치에서_두_칸_움직일_수_있다")
+  void 폰은_초기_위치에서_두_칸_움직일_수_있다(int fromFile, int fromRank, int toFile, int toRank) {
+    // given
+    Pawn pawn = new Pawn(fromRank == 2 ? Color.WHITE : Color.BLACK);
+    Position from = new Position(fromFile, fromRank);
+    Position to = new Position(toFile, toRank);
+
+    // when
+    Path path = pawn.findPath(from, to);
+
+    // then
+    assertThat(path)
+        .extracting("positions", InstanceOfAssertFactories.list(Position.class))
+        .containsExactly(
+            new Position(fromFile, fromRank == 2 ? 3 : 6),
+            to);
+  }
+
+  @ParameterizedTest
+  @CsvSource({
+      "2, 2, 3, 3",
+      "2, 2, 1, 3",
+      "2, 7, 3, 6",
+      "2, 7, 1, 6"
+  })
+  @DisplayName("폰은_대각선으로_한_칸_움직여_적을_공격할_수_있다")
+  void 폰은_대각선으로_한_칸_움직여_적을_공격할_수_있다(int fromFile, int fromRank, int toFile, int toRank) {
+    // given
+    Pawn pawn = new Pawn(fromRank == 2 ? Color.WHITE : Color.BLACK);
+    Position from = new Position(fromFile, fromRank);
+    Position to = new Position(toFile, toRank);
+
+    // when
+    Path path = pawn.findPath(from, to);
+
+    // then
+    assertThat(path)
+        .extracting("positions", InstanceOfAssertFactories.list(Position.class))
+        .containsExactly(to);
+  }
+
+  @Test
+  @DisplayName("폰이_뒤로_움직인다면_예외_처리한다")
+  void 폰이_뒤로_움직인다면_예외_처리한다() {
+    // given
+    Pawn pawn = new Pawn(Color.WHITE);
+    Position from = new Position(2, 3);
+    Position to = new Position(2, 2);
+
+    // when & then
+    assertThatThrownBy(() -> pawn.findPath(from, to))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessageContaining(ErrorMessage.INVALID_DIRECTION.getMessage());
+  }
+
+  @Test
+  @DisplayName("폰이_초기_위치가_아닐_때_두_칸_움직인다면_예외_처리한다")
+  void 폰이_초기_위치가_아닐_때_두_칸_움직인다면_예외_처리한다() {
+    // given
+    Pawn pawn = new Pawn(Color.WHITE);
+    Position from = new Position(2, 3);
+    Position to = new Position(2, 5);
+
+    // when & then
+    assertThatThrownBy(() -> pawn.findPath(from, to))
+        .isInstanceOf(IllegalStateException.class)
+        .hasMessageContaining(ErrorMessage.INVALID_DIRECTION.getMessage());
   }
 }
