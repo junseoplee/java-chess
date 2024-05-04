8c871648471e8865822edafc2a3e3adcbb466b4a
junseoplee <junseoplee@soongsil.ac.kr>
2024-05-04 12:32:22 +0900

    feat: 퀸의 가능한 움직임인지 유효성을 검사하고 경로를 반환하는 기능 구현
    (cherry picked from commit d450116426207daad8a4ca713742982ce0e24629)

--- src/main/java/chess/model/piece/pieces/Queen.java
+++ src/main/java/chess/model/piece/pieces/Queen.java
@@ -1,11 +1,45 @@
 package chess.model.piece.pieces;
 
+import chess.model.movement.Movement;
+import chess.model.movement.Path;
 import chess.model.piece.Piece;
 import chess.model.position.Color;
+import chess.model.position.Position;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Queen extends Piece {
 
+  private static final List<Movement> availableMovements = Arrays.asList(
+      Movement.UP, Movement.DOWN, Movement.LEFT, Movement.RIGHT,
+      Movement.UP_LEFT, Movement.UP_RIGHT, Movement.DOWN_LEFT, Movement.DOWN_RIGHT
+  );
+
   public Queen(final Color color) {
     super(color);
+  }
+
+  @Override
+  public Path findPath(final Position from, final Position to) {
+    Movement movement = to.convertMovement(from);
+    validateMovement(movement, availableMovements);
+
+    return calculatePath(from, to, movement);
+  }
+
+  private Path calculatePath(final Position from, final Position to, final Movement movement) {
+    List<Position> positions = new ArrayList<>();
+
+    Position current = from;
+    positions.add(current);
+
+    while (!current.equals(to)) {
+      current = current.calculateNextPosition(movement);
+      positions.add(current);
+    }
+
+    return new Path(positions);
   }
 }
