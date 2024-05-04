ab641e48f7912c4a9af70411b498775985c9bb5f
junseoplee <junseoplee@soongsil.ac.kr>
2024-05-04 13:06:06 +0900

    feat: 나이트의 가능한 움직임인지 유효성을 검사하고 경로를 반환하는 기능 구현

--- src/test/java/chess/model/piece/pieces/KnightTest.java
+++ src/test/java/chess/model/piece/pieces/KnightTest.java
@@ -1,8 +1,11 @@
 package chess.model.piece.pieces;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import chess.model.position.Color;
+import chess.model.position.Position;
+import chess.view.ErrorMessage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -19,5 +22,19 @@ class KnightTest {
 
     //then
     assertThat(knight.getColor()).isEqualTo(color);
+  }
+
+  @Test
+  @DisplayName("나이트는_L자_형태_외의_방식으로는_움직일_수_없다")
+  void 나이트는_L자_형태_외의_방식으로는_움직일_수_없다() {
+    // given
+    Knight knight = new Knight(Color.WHITE);
+    Position from = new Position(4, 4);
+    Position to = new Position(6, 6);
+
+    // when & then
+    assertThatThrownBy(() -> knight.findPath(from, to))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessageContaining(ErrorMessage.INVALID_DIRECTION.getMessage());
   }
 }
