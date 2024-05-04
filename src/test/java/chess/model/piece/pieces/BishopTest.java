245bbc76edd55c1fec3942df630f179bce4b70df
junseoplee <junseoplee@soongsil.ac.kr>
2024-05-04 12:56:28 +0900

    feat: 비숍의 가능한 움직임인지 유효성을 검사하고 경로를 반환하는 기능 구현

--- src/test/java/chess/model/piece/pieces/BishopTest.java
+++ src/test/java/chess/model/piece/pieces/BishopTest.java
@@ -1,8 +1,11 @@
 package chess.model.piece.pieces;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import chess.model.position.Color;
+import chess.model.position.Position;
+import chess.view.ErrorMessage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -19,5 +22,19 @@ class BishopTest {
 
     //then
     assertThat(bishop.getColor()).isEqualTo(color);
+  }
+
+  @Test
+  @DisplayName("비숍은_상하좌우로는_움직일_수_없다")
+  void 비숍은_상하좌우로는_움직일_수_없다() {
+    // given
+    Bishop bishop = new Bishop(Color.WHITE);
+    Position from = new Position(4, 4);
+    Position to = new Position(4, 6);
+
+    // when & then
+    assertThatThrownBy(() -> bishop.findPath(from, to))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessageContaining(ErrorMessage.INVALID_DIRECTION.getMessage());
   }
 }
