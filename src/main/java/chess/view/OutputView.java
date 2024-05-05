2c1a3e3485398c2bd5cd743c137b04561f6aadfe
junseoplee <junseoplee@soongsil.ac.kr>
2024-05-05 21:37:31 +0900

    feat: 새로운 요구 사항에 맞춰 문구 변경
    (cherry picked from commit 53afee319bda3e228ad996b567defcdc6f0be9fc)

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -17,11 +17,13 @@ public class OutputView {
   private static final int INDEX_OFFSET = 1;
 
   public void printStartMessage() {
-    System.out.println("체스 게임을 시작합니다.");
-    System.out.println("게임 시작은 start, 종료는 end 명령을 입력하세요.");
+    System.out.println("> 체스 게임을 시작합니다.");
+    System.out.println("> 게임 시작 : start");
+    System.out.println("> 게임 종료 : end");
+    System.out.println("> 게임 이동 : move source위치 target위치 - 예. move b2 b3");
   }
 
-  public static void printBoard(final Map<Position, Piece> boardMap) {
+  public void printBoard(final Map<Position, Piece> boardMap) {
     List<List<String>> chessBoard = createEmptyBoard();
     assignSymbols(boardMap, chessBoard);
 
