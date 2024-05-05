70f0d8aa1acffe236811e56da5f322fa4e3f990c
junseoplee <junseoplee@soongsil.ac.kr>
2024-05-05 21:40:02 +0900

    feat: 시작 명령을 입력 받은 후에는 턴에 맞춘 이동 명령과 종료 명령을 받을 수 있도록 구현
    (cherry picked from commit ba83949842b5e9d80515642778e7f5fbed72d173)

--- src/main/java/chess/controller/ChessController.java
+++ src/main/java/chess/controller/ChessController.java
@@ -1,49 +1,58 @@
 package chess.controller;
 
-import chess.model.Command;
+import static chess.view.InputView.END_COMMAND;
+
 import chess.model.board.Board;
 import chess.model.board.InitialBoard;
-import chess.model.piece.Piece;
+import chess.model.position.Color;
 import chess.model.position.Position;
 import chess.view.InputView;
 import chess.view.OutputView;
-import java.util.Map;
+import java.util.List;
 
 public class ChessController {
 
   private final InputView inputView;
   private final OutputView outputView;
-  private final InitialBoard initialBoard;
+  private final Board board;
+  private Color currentTurn;
 
   public ChessController(InputView inputView, OutputView outputView, InitialBoard initialBoard) {
     this.inputView = inputView;
     this.outputView = outputView;
-    this.initialBoard = initialBoard;
+    this.board = initialBoard.createInitialBoard();
+    this.currentTurn = Color.WHITE;
   }
 
   public void runChess() {
     outputView.printStartMessage();
-    processCommand();
-  }
+    String receivedCommand = inputView.receiveStartCommand();
+    if (!receivedCommand.equals(InputView.START_COMMAND)) {
+      return;
+    }
 
-  private void processCommand() {
-    boolean running = true;
+    outputView.printBoard(board.getMap());
 
-    while (running) {
+    while (true) {
       try {
-        Command command = inputView.receiveCommand();
-        Board board = initialBoard.createInitialBoard();
-        Map<Position, Piece> boardMap = board.getMap();
-
-        if ("start".equals(command.getCommand())) {
-          outputView.printBoard(boardMap);
+        List<String> receivedMoveOrEndCommand = inputView.receiveMoveOrEndCommand();
+        if (receivedMoveOrEndCommand.get(0).equals(END_COMMAND)) {
+          break;
         }
-        if ("end".equals(command.getCommand())) {
-          running = false;
-        }
+        Position sourcePosition = parsePosition(receivedMoveOrEndCommand.get(0));
+        Position targetPosition = parsePosition(receivedMoveOrEndCommand.get(1));
+        board.move(sourcePosition, targetPosition, currentTurn);
+        currentTurn = currentTurn.changeTurn(currentTurn);
+        outputView.printBoard(board.getMap());
       } catch (IllegalArgumentException exception) {
         System.out.println(exception.getMessage());
       }
     }
+  }
+
+  private Position parsePosition(String position) {
+    int file = position.charAt(0) - 'a' + 1;
+    int rank = position.charAt(1) - '0';
+    return new Position(file, rank); // 생성자에 1~8 유효성 검사
   }
 }
