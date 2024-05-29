7abb3a05cd6a5c1966fd7f22ca29ecf784f7be04
junseoplee <junseoplee@soongsil.ac.kr>
2024-05-30 00:41:35 +0900

    feat: Piece 별 점수를 추가

--- src/main/java/chess/model/piece/PieceInfo.java
+++ src/main/java/chess/model/piece/PieceInfo.java
@@ -1,5 +1,20 @@
 package chess.model.piece;
 
 public enum PieceInfo {
-  KING, QUEEN, ROOK, BISHOP, KNIGHT, PAWN
+  KING(0),
+  QUEEN(9),
+  ROOK(5),
+  BISHOP(3),
+  KNIGHT(2.5),
+  PAWN(1);
+
+  private final double baseScore;
+
+  PieceInfo(double baseScore) {
+    this.baseScore = baseScore;
+  }
+
+  public double getBaseScore() {
+    return baseScore;
+  }
 }
